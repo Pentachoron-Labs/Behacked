1f296facb0a5bf1c22ac0c354f0b07bf03988a1d
obsequiousnewt <obsequiousnewt@gmail.com>
2016-12-22 16:35:04 -0600

    wow I was pretty bad at coding

--- behacked.c
+++ behacked.c
@@ -1,306 +1,309 @@
-#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
-#include "behacked.h"
+#include <ncurses.h>
 
-int main(int argc, char **argv){
-	int i,n;
-	srandom(time(NULL));
-	initscr(); //initialize ncurses
-	cbreak(); //suspend line buffering
-	noecho(); //@echo off
-	keypad(stdscr,TRUE); //enable arrows
-	start_color(); //enable color
-	for(i=1;i<=6;i++) init_pair(i,i,COLOR_BLACK);
-	mvprintw(4,7,"Behacked");
-	refresh();
-	getch();
-	clear();
-	fill();
-	move(5,5);
-	refresh();
-	while(rmcombs());
-	cx = cy = 5;
-	sx = sy = -1;
-	while((c = getch()) != 3){
-		switch(c){
-			case KEY_LEFT:
-			case 'h':
-				if(cx > 0) cx--;
-				break;
-			case KEY_RIGHT:
-			case 'l':
-				if(cx < WIDTH-1) cx++;
-				break;
-			case KEY_UP:
-			case 'k':
-				if(cy > 0) cy--;
-				break;
-			case KEY_DOWN:
-			case 'j':
-				if(cy < HEIGHT-1) cy++;
-				break;
-#ifdef DEBUG
-			case 'n':
-				redraw();
-				clpendsh();
-				n = 0;
-				ncomb(cx,cy,&n);
-				mvprintw(HEIGHT+4,2,"(%d,%d) %d*%d",cx,cy,n,grid[cy][cx]);
-				refresh();
-				break;
-			case 'N':
-				redraw();
-				mvprintw(2,WIDTH+5,"%d",ncombs());
-				refresh();
-				break;
-			case 'r':
-				redraw();
-				break;
-#endif
-			case ' ':
-				if(sx == -1){
-					sx = cx;
-					sy = cy;
-					attron(COLOR_PAIR(grid[cy][cx]));
-					mvaddch(cy,cx,'*'|A_BOLD);
-					attroff(COLOR_PAIR(grid[cy][cx]));
-				}else{
-					redraw(); //gets rid of bold
-					if((sx-cx == 1 || sx-cx == -1) ^ (sy-cy == 1 || sy-cy == -1)){
-						int r = grid[sy][sx];
-						grid[sy][sx] = grid[cy][cx];
-						grid[cy][cx] = r;
-						redraw();
-						msleep(150);
-						if(rmcombs()){
-							while(rmcombs());
-							if(ncombs() == 0){
-								mvprintw(4,(WIDTH/2)-5,"GAME OVER");
-								getch();
-								endwin();
-								exit(0);
-							}
-						}else{
-							r = grid[sy][sx];
-							grid[sy][sx] = grid[cy][cx];
-							grid[cy][cx] = r;
-							redraw();
-						}
-					}
-					sy = sx = -1;
-				}
-				break;
-#ifdef DEBUG
-			default:
-				mvprintw(HEIGHT+2,5,"%d",c);
-#endif
-		}
-		move(cy,cx);
-		refresh();
-	}
-	endwin();
-	return 0;
+#define	WIDTH	10
+#define HEIGHT	10
+#define DEBUG	1
+
+static int grid[HEIGHT][WIDTH] = {0};
+
+static int _ncomb(int x, int y, int color){
+    int n;
+    
+    if(grid[y][x] != color) return 0;
+    
+    grid[y][x] = 0;
+    n = 1;
+    if(y-1 >= 0)     n += _ncomb(x,y-1,color);
+    if(y+1 < HEIGHT) n += _ncomb(x,y+1,color);
+    if(x-1 >= 0)     n += _ncomb(x-1,y,color);
+    if(x+1 < WIDTH)  n += _ncomb(x+1,y,color);
+    return n;
 }
 
-/**Generates a random integer in the interval [0,n)*/
-int randn(int n){
-	int r;
-	while((r = random()) >= (RAND_MAX/n*n));
-	return (r%n);
+/**
+ * Returns the number of similar gems connected to the specified one,
+ * including said gem.
+ */
+int ncomb(int x, int y){
+    int color = grid[y][x];
+    int i,ret;
+    
+    ret = _ncomb(x,y,color);
+    for(i=0;i<WIDTH*HEIGHT;i++){
+	if(grid[i/WIDTH][i%WIDTH] == 0) grid[i/WIDTH][i%WIDTH] = color;
+    }
+    return ret;
 }
 
-/**Sleeps for a given number of milliseconds*/
+/**
+ * Sleeps for a given number of milliseconds
+ */
 void msleep(int n){
-	nanosleep((struct timespec[]){{0,n*1000000}},NULL);
+    usleep(n*1000);
 }
 
-/**Fills the grid with random gems*/
-void fill(){
-	int i,r;
-	for(i=0;i<WIDTH*HEIGHT;i++){
-		r = randn(6)+1;
-		grid[i/WIDTH][i%WIDTH] = r;
-		attron(COLOR_PAIR(r));
-		mvaddch(i/WIDTH,i%WIDTH,'*');//0x30+r);
-		attroff(COLOR_PAIR(r));
+/**
+ * Just redraws the grid
+ */
+void redraw(void){
+    int i;
+    for(i=0;i<WIDTH*HEIGHT;i++){
+	int r = grid[i/WIDTH][i%WIDTH];
+	if(r != 0){
+	    attron(COLOR_PAIR(r));
+	    mvaddch(i/WIDTH,i%WIDTH,'*');
+	    attroff(COLOR_PAIR(r));
 	}
+    }
+    refresh();
 }
 
-void redraw(){
-	int i;
-	clear();
-	for(i=0;i<WIDTH*HEIGHT;i++){
-		int r = grid[i/WIDTH][i%WIDTH];
-		if(r != 0){
-			attron(COLOR_PAIR(r));
-			mvaddch(i/WIDTH,i%WIDTH,'*');//0x30+r);
-			attroff(COLOR_PAIR(r));
-		}
-	}
-	mvprintw(4,WIDTH+5,"Score: %d",score);
-	refresh();
+/**
+ * Also redraws the current score
+ */
+void redrawall(int score){
+    clear();
+    redraw();
+    mvprintw(4,WIDTH+5,"Score: %d",score);
+    refresh();
 }
 
-/**Clears the shadow grid*/
-void clshadow(){
-	int i;
-	for(i=0;i<WIDTH*HEIGHT;i++){
-		shadow[i/WIDTH][i%WIDTH] = 0;
+void breakcombs(int tobreak[HEIGHT][WIDTH]){
+    int k;
+    for(k=0;k<WIDTH*HEIGHT;k++){
+	if(tobreak[k/WIDTH][k%WIDTH]){
+	    attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
+	    mvaddch(k/WIDTH,k%WIDTH,'o');
 	}
+    }
+    refresh();
+    msleep(150); /*is a nice number*/
+    for(k=0;k<WIDTH*HEIGHT;k++){
+	if(tobreak[k/WIDTH][k%WIDTH]){
+	    attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
+	    mvaddch(k/WIDTH,k%WIDTH,'O');
+	}
+    }
+    refresh();
+    msleep(150);
+    for(k=0;k<WIDTH*HEIGHT;k++){
+	if(tobreak[k/WIDTH][k%WIDTH]){
+	    attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
+	    mvaddch(k/WIDTH,k%WIDTH,' ');
+	    grid[k/WIDTH][k%WIDTH] = 0;
+	}
+    }
+    refresh();
+    msleep(150);
 }
 
-/**Clears the pending shadow grid*/
-void clpendsh(){
-	int i;
-	for(i=0;i<WIDTH*HEIGHT;i++){
-		pendsh[i/WIDTH][i%WIDTH] = 0;
-	}
-}
-
-/**Copies the pending shadow grid to the actual shadow*/
-void acceptpendsh(){
-	int i;
-	for(i=0;i<WIDTH*HEIGHT;i++){
-		shadow[i/WIDTH][i%WIDTH] |= pendsh[i/WIDTH][i%WIDTH];
-	}
-}
-
-/**Removes combinations (groups of four) in the grid and refills; returns 1 if any were found.*/
-int rmcombs(){
-	int x,y,hascombs=0;
-	clshadow();
-	for(y=0;y<HEIGHT;y++){
-		for(x=0;x<WIDTH;x++){
-			int n = 0;
-			clpendsh();
-			ncomb(x,y,&n);
-			if(n >= 4 && grid[y][x] != 0){
-				score += 10;
-				hascombs = 1;
-				acceptpendsh();
-			}
-		}
-	}
-	breakcombs();
-	refill();
-	return hascombs;
-}
-
-/**Returns the number of potential combinations.*/
-int ncombs(){
-	int x,y,n = 0;
-	//test vertical swaps first...
-	for(x=0;x<WIDTH;x++){
-		for(y=0;y<HEIGHT-1;y++){
-			//swap (x,y) and (x,y+1)
-			int r = grid[y][x],q;
-			grid[y][x] = grid[y+1][x];
-			grid[y+1][x] = r;
-			//count the combinations
-			clpendsh();
-			q = 0;
-			ncomb(x,y,&q);
-			n += (q >= 4);
-			clpendsh();
-			q = 0;
-			ncomb(x,y+1,&q);
-			n += (q >= 4);
-			//swap back
-			grid[y+1][x] = grid[y][x];
-			grid[y][x] = r;
-		}
-	}
-	//...and then horizontal
-	for(x=0;x<WIDTH-1;x++){
-		for(y=0;y<HEIGHT;y++){
-			//swap (x,y) and (x+1,y)
-			int r = grid[y][x],q;
-			grid[y][x] = grid[y][x+1];
-			grid[y][x+1] = r;
-			//count the combinations
-			clpendsh();
-			q = 0;
-			ncomb(x,y,&q);
-			n += (q >= 4);
-			clpendsh();
-			q = 0;
-			ncomb(x+1,y,&q);
-			n += (q >= 4);
-			//swap back
-			grid[y][x+1] = grid[y][x];
-			grid[y][x] = r;
-		}
-	}
-	clpendsh();
-	return n;
-}
-
-/**Returns the number of similar gems connected to the specified one (plus one.)*/
-void ncomb(int i, int j, int *n){
-	int q = grid[j][i];
-	pendsh[j][i] = 1;
-	if(j-1 >= 0){
-		if(pendsh[j-1][i] == 0 && grid[j-1][i] == q) ncomb(i,j-1,n);
-	}if(j+1 < HEIGHT){
-		if(pendsh[j+1][i] == 0 && grid[j+1][i] == q) ncomb(i,j+1,n);
-	}if(i-1 >= 0){
-		if(pendsh[j][i-1] == 0 && grid[j][i-1] == q) ncomb(i-1,j,n);
-	}if(i+1 < WIDTH){
-		if(pendsh[j][i+1] == 0 && grid[j][i+1] == q) ncomb(i+1,j,n);
-	}
-	++*n;
-}
-
-void breakcombs(){
-	int k;
-	for(k=0;k<WIDTH*HEIGHT;k++){
-		if(shadow[k/WIDTH][k%WIDTH] == 1){
-			attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
-			mvaddch(k/WIDTH,k%WIDTH,'o');
-		}
-	}
-	refresh();
-	msleep(150); //is a nice number
-	for(k=0;k<WIDTH*HEIGHT;k++){
-		if(shadow[k/WIDTH][k%WIDTH] == 1){
-			attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
-			mvaddch(k/WIDTH,k%WIDTH,'O');
-		}
-	}
-	refresh();
-	msleep(150);
-	for(k=0;k<WIDTH*HEIGHT;k++){
-		if(shadow[k/WIDTH][k%WIDTH] == 1){
-			attron(COLOR_PAIR(grid[k/WIDTH][k%WIDTH]));
-			mvaddch(k/WIDTH,k%WIDTH,' ');
-			grid[k/WIDTH][k%WIDTH] = 0;
-		}
-	}
-	refresh();
-	msleep(150);
+/**
+ * Generates a random integer in the interval (0,n]
+ */
+int randn(int n){
+    int r;
+    while((r = random()) >= (RAND_MAX/n*n));
+    return 1+(r%n);
 }
 
 void refill(void){
-	int hasblank = 0;
-	int x,y;
-	do{
-		hasblank = 0;
-		for(x=0;x<WIDTH;x++){
-			for(y=HEIGHT-1;y>=1;y--){
-				if(grid[y-1][x] != 0 && grid[y][x] == 0){
-					grid[y][x] = grid[y-1][x];
-					grid[y-1][x] = 0;
-					hasblank = 1;
-				}
-			}
-			if(grid[0][x] == 0){
-				grid[0][x] = randn(6)+1;
-				hasblank = 1;
-			}
+    int hasblank;
+    int x,y;
+    do{
+	hasblank = 0;
+	for(x=0;x<WIDTH;x++){
+	    for(y=HEIGHT-1;y>=1;y--){
+		if(grid[y-1][x] != 0 && grid[y][x] == 0){
+		    grid[y][x] = grid[y-1][x];
+		    grid[y-1][x] = 0;
+		    hasblank = 1;
 		}
-		redraw();
-		msleep(150);
-	}while(hasblank == 1);
+	    }
+	    if(grid[0][x] == 0){
+		grid[0][x] = randn(6);
+		hasblank = 1;
+	    }
+	}
+	redraw();
+	msleep(150);
+    }while(hasblank);
+}
+
+/**
+ * Removes combinations (groups of four) in the grid and refills.
+ * Returns the number of points to be added.
+ */
+int rmcombs(void){
+    int tobreak[HEIGHT][WIDTH] = {0};
+    int score=0;
+    int x,y;
+    for(y=0;y<HEIGHT;y++){
+	for(x=0;x<WIDTH;x++){
+	    if(ncomb(x,y) >= 4){
+		tobreak[y][x] = 1;
+		score += 5; /*per gem*/
+	    }
+	}
+    }
+    breakcombs(tobreak);
+    refill();
+    return score;
+}
+
+/**
+ * Returns the number of potential combinations.
+ */
+int ncombs(void){
+    int x,y;
+    int temp;
+    int n;
+    
+    /*test vertical swaps first...*/
+    for(x=0;x<WIDTH;x++){
+	for(y=0;y<HEIGHT-1;y++){
+	    /*swap (x,y) and (x,y+1)*/
+	    temp = grid[y][x];
+	    grid[y][x] = grid[y+1][x];
+	    grid[y+1][x] = temp;
+	    /*count the combinations*/
+	    n += (ncomb(x,y) >= 4);
+	    n += (ncomb(x,y+1) >= 4);
+	    /*swap back*/
+	    grid[y+1][x] = grid[y][x];
+	    grid[y][x] = temp;
+	}
+    }
+    /*...and then horizontal*/
+    for(x=0;x<WIDTH-1;x++){
+	for(y=0;y<HEIGHT;y++){
+	    /*swap (x,y) and (x+1,y)*/
+	    temp = grid[y][x];
+	    grid[y][x] = grid[y][x+1];
+	    grid[y][x+1] = temp;
+	    /*count the combinations*/
+	    n += (ncomb(x,y) >= 4);
+	    n += (ncomb(x+1,y) >= 4);
+	    /*swap back*/
+	    grid[y][x+1] = grid[y][x];
+	    grid[y][x] = temp;
+	}
+    }
+    return n;
+}
+
+int main(int argc, char **argv){
+    int score = 0;
+    int cx,cy; /*cursor x,y*/
+    int sx,sy; /*selected x,y*/
+    int i;
+    int c;
+    
+    srandom(time(NULL)); /*seed random*/
+    initscr(); /*initialize ncurses*/
+    cbreak(); /*suspend line buffering*/
+    noecho(); /*@echo off*/
+    keypad(stdscr,TRUE); /*enable arrows*/
+    start_color(); /*enable color*/
+    for(i=1;i<=6;i++) init_pair(i,i,COLOR_BLACK);
+    
+    mvprintw(4,7,"Behacked");
+    refresh();
+    msleep(2000);
+    clear();
+
+    refill();
+    redrawall(score);
+    
+    move(5,5);
+    refresh();
+    while((i = rmcombs())){
+	score += i;
+	redrawall(score);
+    }
+    cx = cy = 5;
+    sx = sy = -1;
+    while((c = getch()) != 'q'){
+	switch(c){
+	case KEY_LEFT:
+	case 'h':
+	    if(cx > 0) cx--;
+	    break;
+	case KEY_RIGHT:
+	case 'l':
+	    if(cx < WIDTH-1) cx++;
+	    break;
+	case KEY_UP:
+	case 'k':
+	    if(cy > 0) cy--;
+	    break;
+	case KEY_DOWN:
+	case 'j':
+	    if(cy < HEIGHT-1) cy++;
+	    break;
+#ifdef DEBUG
+	case 'n':
+	    redraw();
+	    mvprintw(HEIGHT+4,2,"(%d,%d) %d*%d",cx,cy,ncomb(cx,cy),grid[cy][cx]);
+	    refresh();
+	    break;
+	case 'N':
+	    redraw();
+	    mvprintw(2,WIDTH+5,"%d",ncombs());
+	    refresh();
+	    break;
+	case 'r':
+	    redrawall(score);
+	    break;
+#endif
+	case ' ':
+	    if(sx == -1){
+		sx = cx;
+		sy = cy;
+		attron(COLOR_PAIR(grid[cy][cx]));
+		mvaddch(cy,cx,'*'|A_BOLD);
+		attroff(COLOR_PAIR(grid[cy][cx]));
+	    }else{
+		redraw(); /*gets rid of bold*/
+		if((sx-cx == 1 || sx-cx == -1) ^ (sy-cy == 1 || sy-cy == -1)){
+		    int r = grid[sy][sx];
+		    grid[sy][sx] = grid[cy][cx];
+		    grid[cy][cx] = r;
+		    redrawall(score);
+		    msleep(150);
+		    if((i = rmcombs())){
+			do{
+			    score += i;
+			    redrawall(score);
+			}while((i = rmcombs()));
+			
+			if(ncombs() == 0){
+			    mvprintw(4,(WIDTH/2)-5,"GAME OVER");
+			    getch();
+			    endwin();
+			    exit(0);
+			}
+		    }else{
+			r = grid[sy][sx];
+			grid[sy][sx] = grid[cy][cx];
+			grid[cy][cx] = r;
+			redraw();
+		    }
+		}
+		sy = sx = -1;
+	    }
+	    break;
+#ifdef DEBUG
+	default:
+	    mvprintw(HEIGHT+2,5,"%d",c);
+#endif
+	}
+	move(cy,cx);
+	refresh();
+    }
+    endwin();
+    return 0;
 }
